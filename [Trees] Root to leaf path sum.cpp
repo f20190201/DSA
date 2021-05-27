81ed76855bcf24e98cde81109d9b27aeaaea79aa
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-05-27 16:08:03 +0530

    Create [Trees] Root to leaf path sum.cpp

---
+++ [Trees] Root to leaf path sum.cpp
@@ -0,0 +1,21 @@
+// We check if the right or left subtree has a sum of (S - curr_node->data), if yes, then return 1
+// If we reach the leaf, and S isn't equal to leaf->data, return 0, else return 1
+
+bool hasPathSum(Node *root, int S) {
+    
+    
+    if((root->left == NULL) && (root->right == NULL)){
+       return(S == root->data);
+    }
+       bool left = 0;
+       bool right = 0;
+        
+    if(root->left != NULL)
+        left = left || hasPathSum(root->left , S-(root->data));
+    
+    if(root->right != NULL)
+        right = right || hasPathSum(root->right , S-(root->data));
+    
+        return (right || left);
+    
+}
