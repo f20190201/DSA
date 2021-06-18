8d386452457a022fe52bfdfa94fc34ee31861382
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-06-18 17:54:41 +0530

    Update reverse-integer.cpp

--- reverse-integer/reverse-integer.cpp
+++ reverse-integer/reverse-integer.cpp
@@ -30,3 +30,50 @@ public:
         
     }
 };
+
+
+
+-----------------------------------------------------------------------
+    
+    class Solution {
+public:
+    int reverse(int x) {
+        
+        if(x>INT_MAX || x<INT_MIN) return 0;
+        
+        int b=0;
+        int a=abs(x);
+        while(a>0)
+        {
+            if(b > INT_MAX/10) 
+                return 0;
+            b=b*10 + a%10;
+            a=a/10;
+        }
+        
+        if(x<0)
+        {
+            return b*(-1);
+        }
+        return b;
+    }
+};
+
+
+---------------------------------------------------------------------
+    
+    
+    class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
+            if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+};
