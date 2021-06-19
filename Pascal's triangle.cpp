084c9a08224cc838448159097a70eace76887810
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-06-20 02:03:04 +0530

    Update Pascal's triangle.cpp

--- Pascal's triangle.cpp
+++ Pascal's triangle.cpp
@@ -23,7 +23,7 @@ void printPascal(int n){
         int c = 1;
         cout<<c<<" ";
         
-        for(int j = 0 ; j <= i ; j++){
+        for(int j = 1 ; j <= i ; j++){
             c = c*(i-j+1)/j;
             cout<<c<<" ";
         }
