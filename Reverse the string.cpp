91e7ff67ebc13db0b6569984c42c8f6a41dd5056
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-06-28 18:11:12 +0530

    Update Reverse the string.cpp

--- Reverse the string.cpp
+++ Reverse the string.cpp
@@ -35,3 +35,43 @@ string Solution::solve(string A) {
     return ans.substr(0 , ans.size() - 1);
 
 }
+
+
+---------------------------------------------------------------------------------
+// Here, instead of a vector we first append the space, then our current word and finally the answer till now.
+// This way this current word is appended to the left instead of right
+    
+  
+    string Solution::solve(string A) {
+    int i = 0;
+    int k = A.size() - 1;
+
+    while(A[k] == ' ')
+        k--;
+
+    while(A[i] == ' ')
+        i++;
+
+    string ans = "";
+
+    for(int j = i ; j <= k ; j++){
+        string word = "";
+        
+        while(A[j] == ' ')
+            j++;
+
+        while(j <= k and A[j] != ' '){
+            word += A[j];
+            j++;
+        }
+        ans = " " + word + ans;
+    }
+    
+
+    
+    return ans.substr(1);
+
+}
+
+
+
