748c482b7849ee0a187d3eb9cbd563e38aff1d7d
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-07-03 00:26:21 +0530

    Update [LL] Subtract values.cpp

--- [LL] Subtract values.cpp
+++ [LL] Subtract values.cpp
@@ -39,3 +39,65 @@ ListNode* Solution::subtract(ListNode* A) {
     return A;
 
 }
+
+
+------------------------------------------------------------
+    
+    
+    
+    ListNode* reverseList(ListNode* head) {
+	ListNode* prev = NULL;
+	ListNode* cur = head;
+	ListNode* next;
+
+	while (cur != NULL) {
+		next = cur->next;
+		cur->next = prev;
+		prev = cur;
+		cur = next;
+	}
+
+	return prev;
+}
+
+ListNode* Solution::subtract(ListNode* head) {
+
+	// Zero or single node
+	if (head == NULL || head->next == NULL)
+		return head;
+
+	ListNode* slow = head;
+	ListNode* fast = head->next;
+
+	while (fast->next != NULL && (fast->next)->next != NULL) {
+		slow = slow->next;
+		fast = (fast->next)->next;
+	}
+
+	// Now slow points to the last node to be updated i.e. n/2th node
+
+	// Reverse the remaining list
+	// Tail points to the last node of the original list but this list is now in reverse direction.
+	ListNode* tail = reverseList(slow->next);
+
+
+	// Keep updating values of the first half of the list till n/2th node i.e. till curHead reaches slow
+	ListNode* curHead = head;
+	ListNode* curTail = tail;
+	while (true) {
+		curHead->val = curTail->val - curHead->val;
+		if (curHead == slow) {
+			// First n/2 values have been updated.
+			break;
+		}
+		curHead = curHead->next;
+		curTail = curTail->next;
+	}
+
+	// Values have been updated now.
+
+	// Now just join back the list that was reverse after the slow node i.e. while computing tail node.
+	slow->next = reverseList(tail);
+
+	return head;
+}
