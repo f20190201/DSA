eaada2c920774935380c8352c617f833bbfa4a14
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-07-09 18:26:27 +0530

    Update [Greedy] Gas Station.cpp

--- [Greedy] Gas Station.cpp
+++ [Greedy] Gas Station.cpp
@@ -19,3 +19,29 @@ int Solution::canCompleteCircuit(const vector<int> &A, const vector<int> &B) {
         return start;
     return -1;
 }
+
+---------------------------------------------------------------------------------------------------------------------------------------
+
+class Solution {
+    public:
+        int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+            int sumGas = 0;
+            int sumCost = 0;
+            int start = 0;
+            int tank = 0;
+            for (int i = 0; i < gas.length; i++) {
+                sumGas += gas[i];
+                sumCost += cost[i];
+                tank += gas[i] - cost[i];
+                if (tank < 0) {
+                    start = i + 1;
+                    tank = 0;
+                }
+            }
+            if (sumGas < sumCost) {
+                return -1;
+            } else {
+                return start;
+            }
+        }
+};
