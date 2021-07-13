8a45d4d44c28526b2395ec3f071be1fb93aceacf
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-07-13 08:35:26 +0530

    Update [Trees] Build tree from Inorder and postorder.cpp

--- [Trees] Build tree from Inorder and postorder.cpp
+++ [Trees] Build tree from Inorder and postorder.cpp
@@ -17,13 +17,13 @@ int search(int inorder[] , int start , int end , int curr){
 }
 
 
-Node* buildTree(int preorder[] , int inorder[] , int start , int end){
+Node* buildTree(int postorder[] , int inorder[] , int start , int end){
     static int idx = n;   // n is the length of the array
     
     if(start > end)
         return NULL;
     
-    int curr = preorder[idx];
+    int curr = postorder[idx];
     idx--;
     Node* node = new Node(curr);
     
@@ -31,8 +31,8 @@ Node* buildTree(int preorder[] , int inorder[] , int start , int end){
         return node;
     
     int pos = search(inorder , start , end , curr);
-    node->right = buildTree(preorder , inorder , pos+1 ,end);
-    node->left = buildTree(preorder , inorder , start ,pos-1);
+    node->right = buildTree(postorder , inorder , pos+1 ,end);
+    node->left = buildTree(postorder , inorder , start ,pos-1);
     
     
     return node;
