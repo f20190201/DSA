0fd3560d267737a47d86a71f7f820963d6ee1c45
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-07-19 21:15:39 +0530

    Create [DP] Kingdom War.cpp

---
+++ [DP] Kingdom War.cpp
@@ -0,0 +1,22 @@
+// Since the 2D array is row and column sorted, our task is to find the amsximum sum subarray
+// The subarray would always include the last element since it is the largest
+
+int Solution::solve(vector<vector<int> > &A) {
+    int n = A.size() , m = A[0].size();
+    int dp[n + 1][m + 1];
+    memset(dp , 0 , sizeof(dp));
+
+    int res = INT_MIN;
+
+    for(int i = n - 1 ; i >= 0 ; i--){
+        for(int j = m - 1 ; j >= 0 ; j--){
+
+            dp[i][j] = A[i][j] + (dp[i + 1][j] + dp[i][j + 1] - dp[i + 1][j + 1]);
+
+            res = max(res , dp[i][j]);
+        }
+
+    }
+
+    return res;
+}
