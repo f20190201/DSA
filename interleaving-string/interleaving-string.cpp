de614069a69f7eb211da6e4ff34b20919f1fb555
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-07-26 23:39:47 +0530

    Update interleaving-string.cpp

--- interleaving-string/interleaving-string.cpp
+++ interleaving-string/interleaving-string.cpp
@@ -30,3 +30,48 @@ public:
         return solve(s1 , s2 , s3 , 0 , 0 , 0);
     }
 };
+
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+    
+class Solution {
+public:
+    int dp[201][201];
+    
+    bool isInterleave(string s1, string s2, string s3) {
+        
+        if((s1.size() + s2.size()) != s3.size())
+            return 0;
+        
+        memset(dp , -1 , sizeof(dp));
+        
+        dp[0][0] = 1;
+        
+        for(int i = 1 ; i <= s1.size() ; i++){
+            dp[i][0] = (s3[i - 1] == s1[i - 1]) ? dp[i - 1][0] : 0;
+        }
+        
+        for(int j = 1 ; j <= s2.size() ; j++)
+            dp[0][j] = (s3[j - 1] == s2[j - 1]) ? dp[0][j - 1] : 0;
+        
+        for(int i = 1 ; i <= s1.size() ; i++){
+            for(int j = 1 ; j <= s2.size() ; j++){
+                
+                if(s1[i - 1] == s3[i + j - 1] && s2[j - 1] == s3[i + j - 1])
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                
+                else if(s1[i - 1] == s3[i + j - 1])
+                    dp[i][j] = dp[i - 1][j];
+                
+                else if(s2[j - 1] == s3[i + j - 1])
+                    dp[i][j] = dp[i][j - 1];
+                
+                else
+                    dp[i][j] = 0;
+                
+            }
+        }
+        return dp[s1.size()][s2.size()];
+    }
+};
