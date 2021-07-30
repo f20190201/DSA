29fc8107208aa35771deb45b798bb396e9da159f
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-07-30 10:01:42 +0530

    Update insert-interval.cpp

--- insert-interval/insert-interval.cpp
+++ insert-interval/insert-interval.cpp
@@ -30,3 +30,49 @@ public:
         
     }
 };
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& A, vector<int>& add) {
+        // A.push_back(add);
+        // sort(A.begin() , A.end());
+        
+        int i = 0;
+        
+        while(i < A.size() && A[i][0] < add[0])
+            i++;
+        
+        A.insert(A.begin() + i , add);
+        
+        
+        vector<vector<int>> result;
+        
+        int start = A[0][0] , end = A[0][1];
+        
+        for(int i = 1 ; i < A.size() ; i++){
+            
+            while(i < A.size() && A[i][0] <= end){
+                end = max(end , A[i][1]);
+                i++;
+            }
+            
+            if(i == A.size())
+                break;
+            
+            result.push_back({start , end});
+            
+            start = A[i][0];
+            end = A[i][1];
+        }
+        
+        result.push_back({start , end});
+        
+        return result;
+        
+        
+    }
+};
