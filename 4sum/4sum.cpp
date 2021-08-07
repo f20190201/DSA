1acb48dcec59d99224f24c36d2eeadaa1d097aca
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-08-07 10:58:19 +0530

    Time: 40 ms (82.40%), Space: 13 MB (85.42%) - LeetHub

--- 4sum/4sum.cpp
+++ 4sum/4sum.cpp
@@ -1,47 +1,51 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& A, int target) {
+        
         vector<vector<int>> result;
         
         sort(A.begin() , A.end());
         
-        for(int i = 0 ; i < A.size() ; i++){
+        for(int i = 0 ; i < A.size() - 1 ; i++){
             int t1 = target - A[i];
             
-            for(int j = i + 1 ; j < A.size() ; j++){
+            for(int j = i + 1 ; j < A.size() - 1 ; j++){
                 int t2 = t1 - A[j];
                 
-                int l = j + 1 , r = A.size() - 1;
+                int low = j + 1 , high = A.size() - 1;
                 
-                while(l < r){
-                    int sum = A[l] + A[r];
+                while(low < high){
+                    int sum = A[low] + A[high];
                     
                     if(sum > t2)
-                        r--;
-                    else if(sum < t2)
-                        l++;
+                        high--;
+                    
+                    else if (sum < t2)
+                        low++;
                     
                     else{
-                        vector<int> temp(4);
-                        temp[0] = A[i];
-                        temp[1] = A[j];
-                        temp[2] = A[l];
-                        temp[3] = A[r];
-                        result.push_back(temp);
+                        vector<int> quad(4);
+                        quad[0] = A[i];
+                        quad[1] = A[j];
+                        quad[2] = A[low];
+                        quad[3] = A[high];
                         
-                        while(l < r and A[l] == temp[2])
-                            l++;
-                        while(l < r and A[r] == temp[3])
-                            r--;
+                        result.push_back(quad);
+                        
+                        while(low < A.size() && A[low] == quad[2])
+                            low++;
+                        
+                        while(high < A.size() && A[high] == quad[3])
+                            high--;
                     }
-                        
                 }
-                while(j + 1 < A.size() and A[j] == A[j + 1])
+                
+                while(j < A.size() - 1 && A[j] == A[j + 1])
                     j++;
             }
+            while(i < A.size() - 1 && A[i] == A[i + 1])
+                    i++;
             
-            while(i + 1 < A.size() and A[i] == A[i + 1])
-                i++;
         }
         return result;
     }
