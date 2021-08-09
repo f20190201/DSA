41499540336969a1a3fd79bb46ed3f8ba32014d0
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-08-09 16:23:11 +0530

    Time: 8 ms (71.32%), Space: 11 MB (57.28%) - LeetHub

--- combination-sum/combination-sum.cpp
+++ combination-sum/combination-sum.cpp
@@ -2,22 +2,26 @@ class Solution {
 public:
     vector<vector<int>> result;
     
-    void solve(vector<int>&A , vector<int> &v , int sum , int target , int index , int n){
+    int t;
+    
+    void solve(vector<int>&A , vector<int> &v , int sum , int index , int n){
         if(index == n){
-            if(sum == target)
+            if(sum == t)
                 result.push_back(v);
             return;
         }
         
-        if(sum > target)
+        if(sum > t)
             return;
         
-        sum = sum + A[index];
+        
         v.push_back(A[index]);
-        solve(A , v , sum , target , index , n);
+        
+        solve(A , v , sum + A[index] , index , n);
+        
         v.pop_back();
-        sum = sum - A[index];
-        solve(A , v , sum , target , index + 1 , n);
+
+        solve(A , v , sum , index + 1 , n);
         
         return;
         
@@ -26,10 +30,10 @@ public:
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         result.clear();
-        
+        t = target;
         vector<int> v;
         
-        solve(candidates , v , 0 , target , 0 , candidates.size());
+        solve(candidates , v , 0 , 0 , candidates.size());
         
         return result;
     }
