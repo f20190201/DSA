a4ceb3fe8dcf81c6faf9885ba08dad1c7309dbd8
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-08-09 10:20:37 +0530

    Update remove-duplicates-from-sorted-array.cpp

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -19,3 +19,25 @@ public:
         return unique + 1;
     }
 };
+
+
+-----------------------------------------------------------------------------------------------
+    
+    
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() == 0)
+            return 0;
+        
+        int unique = 0;
+        
+        for(int i = 1 ; i < nums.size(); i++){
+            
+            if(nums[unique] != nums[i]){
+                unique++;
+                nums[unique] = nums[i];
+            }
+            
+        }
+        
+        return unique + 1;
+    }
