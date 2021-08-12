5f40e8d34d5af8b63cdaf8649e8b08fbd0b639ba
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-08-12 07:38:47 +0530

    Time: 8 ms (76.53%), Space: 11.1 MB (38.93%) - LeetHub

--- single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,55 +1,26 @@
 class Solution {
 public:
-    int firstOcc(vector<int>& A , int target){
-        int low = 0 , high = A.size() - 1 , res = -1;
-        
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            
-            if(A[mid] > target)
-                high = mid - 1;
-            
-            else if(A[mid] < target)
-                low = mid + 1;
-            
-            else{
-                res = mid;
-                high = mid - 1;
-            }
-        }
-        
-        return res;
-    }
-    
-    int lastOcc(vector<int>& A , int target){
-        int low = 0 , high = A.size() - 1 , res = A.size();
-        
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            
-            if(A[mid] > target)
-                high = mid - 1;
-            
-            else if(A[mid] < target)
-                low = mid + 1;
-            
-            else{
-                res = mid;
-                low = mid + 1;
-            }
-        }
-        
-        return res;
-    }
-    
     int singleNonDuplicate(vector<int>& nums) {
+        int low = 0 , high = nums.size() - 2;
         
-        for(int i = 0 ; i < nums.size() ; i++){
+        while(low <= high){
+            int mid = low + (high - low) / 2;
             
-            if(firstOcc(nums , nums[i]) == lastOcc(nums , nums[i]))
-                return nums[i];
+            if(mid % 2){
+                if(nums[mid] == nums[mid - 1])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+            
+            else{
+                if(nums[mid] == nums[mid + 1])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
         }
         
-        return -1;
+        return nums[low];
     }
 };
