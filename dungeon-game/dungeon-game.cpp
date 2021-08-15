d6e3fd3f143159e11fefb35c6b10c78366466b85
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-08-15 15:59:00 +0530

    Time: 8 ms (55.74%), Space: 9.1 MB (6.21%) - LeetHub

--- dungeon-game/dungeon-game.cpp
+++ dungeon-game/dungeon-game.cpp
@@ -1,38 +1,45 @@
 class Solution {
 public:
+    vector<vector<int>> A;
+    int n , m;
+    
     int dp[201][201];
-    vector<vector<int>>A;
     
     int solve(int i , int j){
         
-        if(i == A.size() || j == A[0].size())
-            return INT_MIN / 10;
+        if(i < 0 || i >= n || j < 0 || j >= m)
+            return INT_MIN;
         
-        if(dp[i][j] != -1)
+        if(dp[i][j] != 1001)
             return dp[i][j];
         
-        // cost = min(A[i][j] , cost + A[i][j]);
-        
-        if(i == A.size() - 1 && j == A[0].size() - 1){
+        if(i == n - 1 && j == m - 1)
             return dp[i][j] = A[i][j];
-        }
         
-        
-        int right = solve(i , j + 1);
         int down = solve(i + 1 , j);
+        int right = solve(i , j + 1);
         
-        return dp[i][j] =  min(A[i][j] , A[i][j] + max(right , down));
+        return dp[i][j] = min(A[i][j] , A[i][j] + max(down , right));
+        
         
     }
     
+    
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        A = dungeon;
-        memset(dp , -1 , sizeof(dp));
+        A = dungeon;   
+        n = dungeon.size();
+        m = dungeon[0].size();
+        
+        for(int i = 0 ; i < 201 ; i++){
+            for(int j = 0 ; j < 201 ; j++)
+                dp[i][j] = 1001;
+        }
+        
         int ans = solve(0 , 0);
+        
         if(ans > 0)
             return 1;
-        return 1 - ans;
-    
         
+        return (1 - ans);
     }
 };
