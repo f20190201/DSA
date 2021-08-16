0e0d3de329fc157521954e32c1d812e6abf7c7de
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-08-16 12:48:04 +0530

    Time: 48 ms (95.61%), Space: 48.4 MB (20.14%) - LeetHub

--- jump-game/jump-game.cpp
+++ jump-game/jump-game.cpp
@@ -1,35 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
+        int n = nums.size() , start = n - 1;
         
-        if(n == 1)
-            return 1;
-        
-        int dp[n + 1];
-        memset(dp , 0 , sizeof(dp));
-        
-        dp[0] = 1;
-        
-        for(int i = 0 ; i < n ; i++){
-            
-            if(dp[i] == 0)
-                return 0;
-                
-            int jump = nums[i];
-            
-            
-            while(jump >= 0){
-                
-                if(i + jump >= n)
-                    return 1;
-                
-                else
-                    dp[i + jump] = 1;
-                
-                jump--;
-            }
+        for(int i = n - 2 ; i >= 0 ; i--){
+            if(nums[i] + i >= start)
+                start = i;
         }
-        return dp[n-1];
+        
+        return (start <= 0);
     }
 };
