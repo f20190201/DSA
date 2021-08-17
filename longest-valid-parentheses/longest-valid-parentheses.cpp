49b3f01bc0d8ccf128c7d849a1b701d2834742fd
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-08-17 14:02:07 +0530

    Time: 4 ms (78.90%), Space: 6.7 MB (99.29%) - LeetHub

--- longest-valid-parentheses/longest-valid-parentheses.cpp
+++ longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int n = s.size() , count = 0 , ans = 0;
+        int n = s.size() , count = 0;
         
-        for(int i = 0 ; i < n ; i++){
-            count = 0;
+        if(n <= 1)
+            return 0;
+        
+        int dp[n];
+        memset(dp , 0 , sizeof(dp));
+        
+        for(int i = 1 ; i < n ; i++){
             
-            for(int j = i ; j < n ; j++){
-                
-                if(s[j] == '(')
-                    count++;
-                
-                else
-                    count--;
-                
-                if(!count)
-                    ans = max(ans , j - i + 1);
-                
-                else if(count < 0)
-                    break;
+            if(s[i] == '(')
+                continue;
+            
+            if(s[i - 1] == '(')
+                dp[i] = 2 + ((i - 2) >= 0 ? dp[i - 2] : 0);
+            
+            else{
+                if((i - dp[i - 1] - 1) >= 0 && s[i - dp[i - 1] - 1] == '(')
+                    dp[i] = 2 + dp[i - 1] + ((i - dp[i - 1] - 2) >= 0 ? dp[i - dp[i - 1] - 2] : 0);
             }
+            
+            count = max(count , dp[i]);
         }
         
-        return ans;
+        return count;
     }
 };
