18317106bfefd6a20ae6c3ebd786930c7c216537
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-08-17 12:32:04 +0530

    Time: 1692 ms (27.57%), Space: 244.5 MB (19.00%) - LeetHub

--- longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
+++ longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
     int longestArithSeqLength(vector<int>& nums) {
-        int n = nums.size() , maxLen = 0;
+        int n = nums.size() , ans = 2;
         
-        vector<map<int , int>> sequences(n);    // map stores differences as key and their existence as values
+        if(n <= 2)
+            return n;
+        
+        vector<unordered_map<int , int>>mp(n);
+        
+        vector<int>dp(n , 1);
         
         for(int i = 0 ; i < n ; i++){
-            for(int j = i + 1 ; j < n ; j++){
+            for(int j = 0 ; j < i ; j++){
                 
-                int diff = nums[j] - nums[i];   // implying that ... , A[i] , A[j] , ... form AP
+                int diff = nums[i] - nums[j];
                 
-                if(sequences[i].find(diff) != sequences[i].end())
-                    sequences[j][diff] = sequences[i][diff] + 1;
+                if(mp[j].count(diff)){
+                    
+                    dp[i] = max(dp[i] , 1 + mp[j][diff]);
+                    
+                    mp[i][diff] = 1 + mp[j][diff];
+                    
+                }
                 
                 else
-                    sequences[j][diff] = 2;
-                
-                maxLen = max(maxLen , sequences[j][diff]);
+                    mp[i][diff] = 2;
             }
         }
         
-        return maxLen;
+        for(auto x : dp)
+            ans = max(ans , x);
+        
+        return ans;
     }
 };
