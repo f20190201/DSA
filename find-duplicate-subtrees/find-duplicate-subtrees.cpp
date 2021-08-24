4096b8c03185a2a781cf131ad84596ced3d6c755
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-08-24 22:27:15 +0530

    Time: 48 ms (31.70%), Space: 47.6 MB (64.13%) - LeetHub

---
+++ find-duplicate-subtrees/find-duplicate-subtrees.cpp
@@ -0,0 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    map<string , pair<TreeNode* , int>> mp;
+    
+    string solve(TreeNode* root){
+        if(root == NULL)
+            return "@";
+        
+        string s = "";
+        
+        if(root->left == NULL && root->right == NULL){
+            
+            s = to_string(root->val);
+            
+            if(mp.count(s)){
+            mp[s].second++;
+            }
+            
+            else{
+                mp[s].first = root;
+                mp[s].second = 1;
+            }
+            
+            return s;
+        }
+        
+        s += to_string(root->val);
+        s += " ";
+        s += solve(root->left);
+        s += " ";
+        s += solve(root->right);
+        
+        if(mp.count(s)){
+            mp[s].second++;
+        }
+        else{
+            mp[s].first = root;
+            mp[s].second = 1;
+        }
+        
+        return s;
+            
+    }
+    
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        mp.clear();
+        
+        solve(root);
+        
+        vector<TreeNode*> res;
+        
+        for(auto it = mp.begin() ; it != mp.end() ; ++it){
+            if(it->second.second >= 2)
+                res.push_back(it->second.first);
+        }
+        
+        return res;
+    }
+};
