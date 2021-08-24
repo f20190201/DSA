fa91634db01c107b8ee9c77927e6f29ca9c5ec58
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-08-24 22:55:24 +0530

    Update construct-string-from-binary-tree.cpp

--- construct-string-from-binary-tree/construct-string-from-binary-tree.cpp
+++ construct-string-from-binary-tree/construct-string-from-binary-tree.cpp
@@ -54,3 +54,16 @@ public:
         return s;
     }
 };
+
+
+-----------------------------------------------------------
+    
+    
+    string tree2str(TreeNode* t) {
+        if (t == NULL) return "";
+        string s = to_string(t->val);
+        if (t->left) s += '(' + tree2str(t->left) + ')';
+        else if (t->right) s += "()";
+        if (t->right) s += '(' + tree2str(t->right) + ')';
+        return s;
+    }
