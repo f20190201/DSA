7bb6ce5e6081a12079c0f992588c7e4056f6fab6
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-08-25 08:33:34 +0530

    Update flatten-binary-tree-to-linked-list.cpp

--- flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -42,3 +42,24 @@ public:
         return;
     }
 };
+
+
+----------------------------------------------------------------------------------
+    
+    
+    TreeNode* prev = NULL;
+    void flatten(TreeNode* root) {
+        
+        if(root == NULL)
+            return;
+        
+        flatten(root->right);
+        flatten(root->left);
+        
+        root->right = prev;
+        root->left = NULL;
+        
+        prev = root;
+        
+        return;
+    }
