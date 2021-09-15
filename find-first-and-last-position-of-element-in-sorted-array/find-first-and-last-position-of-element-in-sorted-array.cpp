5f38622bb4c080422f13ba517118607531732390
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-09-15 22:58:50 +0530

    Time: 23 ms (5.04%), Space: 13.5 MB (85.42%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,50 +1,13 @@
 class Solution {
 public:
-    int first(vector<int>& A , int target){
-        int l = 0 , r = A.size() - 1 , res = -1;
-        
-        while(l <= r){
-            int mid = (l + r) / 2;
-            
-            if(A[mid] == target){
-                res = mid;
-                r = mid - 1;
-            }
-            
-            else if(A[mid] > target)
-                r = mid - 1;
-            
-            else
-                l = mid + 1;
-                
-        }
-        return res;
-        
-    }
-    
-    int last(vector<int>& A , int target){
-        int l = 0 , r = A.size() - 1 , res = -1;
-        
-        while(l <= r){
-            int mid = (l + r) / 2;
-            
-            if(A[mid] == target){
-                res = mid;
-                l = mid + 1;
-            }
-            
-            else if(A[mid] > target)
-                r = mid - 1;
-            
-            else
-                l = mid + 1;
-                
-        }
-        return res;
-        
-    }
-    
     vector<int> searchRange(vector<int>& nums, int target) {
-        return {first(nums , target) , last(nums , target)};
+        
+        if(binary_search(nums.begin() , nums.end() , target) == 0)
+            return {-1 , -1};
+        
+        int first = lower_bound(nums.begin() , nums.end() , target) - nums.begin();
+        int last = upper_bound(nums.begin() , nums.end() , target) - nums.begin();
+        
+        return {first , last - 1};
     }
 };
