13eba2c02ae4f486e02b9f444dabeb92d4fa0de0
Subham Patel <66865445+f20190201@users.noreply.github.com>
2021-09-26 11:12:56 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (24.33%) - LeetHub

---
+++ integer-break/integer-break.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:   
+    int integerBreak(int n) {
+        vector<int> dp(n + 1 , 1);
+        
+        for(int i = 3 ; i <= n ; i++){
+            for(int j = 1 ; j < i ; j++){
+                dp[i] = max(dp[i] , max({(i - j) * j , dp[i - j] * dp[j] , dp[i - j] * j}));
+            }
+        }
+        
+        return dp[n];
+    }
+};
