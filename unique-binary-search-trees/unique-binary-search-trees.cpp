368acaf0801e8aaa0e952feb5511278f5df5e894
Subham Patel <66865445+f20190201@users.noreply.github.com>
2022-01-14 11:36:15 +0530

    Update unique-binary-search-trees.cpp

--- unique-binary-search-trees/unique-binary-search-trees.cpp
+++ unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,3 +1,4 @@
+// Catalan Numbers method (2nCn)/(n + 1)
 class Solution {
 public:
     
@@ -18,5 +19,28 @@ public:
         ans = binomialCoeff(2 * n , n);
         
         return ans/(n + 1);
+    }
+};
+
+//O(n^2) method
+// Result can be obtained by summing of all the sub-results which are obtained by keeping each value from 1 to n as root
+
+class Solution {
+public:
+    int numTrees(int n) {
+        
+        int dp[n + 1];
+        memset(dp , 0 , sizeof(dp));
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2 ; i <= n ; i++){  //keeping each value from 2 to n as root
+            
+            for(int j = 1 ; j <= i ; j++)   //for each root value i, keeping all the values from 1 to i as root
+                dp[i] += dp[j - 1] * dp[i - j];
+        }
+        
+        return dp[n];
     }
 };
