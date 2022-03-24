cab88f21834086b90790cfa73b884e3fb97f9e6b
Subham Patel <66865445+f20190201@users.noreply.github.com>
2022-03-24 10:49:24 +0530

    Time: 13 ms (34.39%), Space: 13.7 MB (72.45%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int globalMax = nums[0] , tempMax = nums[0] , tempMin = nums[0];
+//         int globalMax = nums[0] , tempMax = nums[0] , tempMin = nums[0];
         
-        for(int i = 1 ; i < nums.size() ; i++){
+//         for(int i = 1 ; i < nums.size() ; i++){
             
-            int temp = tempMax;
+//             int temp = tempMax;
             
-            tempMax = max({nums[i] , tempMax*nums[i] , tempMin*nums[i]});
+//             tempMax = max({nums[i] , tempMax*nums[i] , tempMin*nums[i]});
             
-            globalMax = max(globalMax , tempMax);
+//             globalMax = max(globalMax , tempMax);
             
-            tempMin = min({nums[i] , temp*nums[i] , tempMin*nums[i]});
+//             tempMin = min({nums[i] , temp*nums[i] , tempMin*nums[i]});
+//         }
+        
+        int left = 0 , right = 0 , globalMax = nums[0];
+        
+        for(int i = 0 ; i < nums.size() ; i++){
+            left = (left != 0 ? left : 1)*nums[i];
+            right = (right != 0 ? right : 1)*nums[nums.size() - 1 - i];
+            
+            globalMax = max({globalMax , left , right});
         }
+        
+        
         
         return globalMax;
     }
