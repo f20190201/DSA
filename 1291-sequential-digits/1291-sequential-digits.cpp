6bbc5dc60503802c35df2429e56442264f5f020d
Subham Patel <66865445+f20190201@users.noreply.github.com>
2022-08-05 21:42:50 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (17.20%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int countDigits(int n){
+        
+        return floor(1 + log10(n));
+        
+    }
+    
+    int generateFirstNum(int digits){
+        string s = "";
+        
+        for(int i = 1 ; i <= digits ; i++)
+            s += to_string(i);
+        
+        return stoi(s);
+    }
+    
+    int generateNextNum(int n){
+        string s = to_string(n);
+        
+        for(int i = 0 ; i < s.size() ; i++)
+            s[i] = char(int(s[i]) + 1);
+        
+        return stoi(s);
+    }
+    
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int>result;
+        
+        int startDigits = countDigits(low) , endDigits = min(countDigits(high) , 9);
+        
+        int currDigits = startDigits , num = generateFirstNum(currDigits);
+        
+        while(num <= high){
+            
+            if(num >= low)
+                result.push_back(num);
+            
+            if(num % 10 == 9){
+                if(currDigits < 9)
+                    num = generateFirstNum(++currDigits);
+                else
+                    break;
+            }
+            
+            else{
+                num = generateNextNum(num);
+            }
+        }
+        
+        return result;
+    }
+};
